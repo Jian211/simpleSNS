ab29c8a3725938cd010a5bc480bf91a5fadddf50
jian <myunggeun22@gmail.com>
2023-03-16 22:49:49 +0900

    メッセージCSS作成

--- client/components/MsgItem.js
+++ client/components/MsgItem.js
@@ -6,12 +6,13 @@ const MsgItem = ({userId, timestamp, text}) => (
             {userId}{' '}
             <sub>
                 {new Date(timestamp).toLocaleString('ko-kr',{
+                    weekday: "long",
                     year: 'numeric',
                     month:"numeric",
                     day: "numeric",
                     hour:"2-digit",
                     minute:"2-digit",
-                    hour12:true
+                    hour12:true,
                 })}
             </sub>
         </h3>
